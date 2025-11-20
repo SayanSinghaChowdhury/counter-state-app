380a2dee73c457e8763e9cb73d42ad423ba3acba
Mr Sayan <sayansinghachowdhury.dev@gmail.com>
2025-11-20 14:11:52 +0530

    Basic counter done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
-import { Button } from "@/components/shadcnui/button";
-import {
-	Card,
-	CardContent,
-	CardHeader,
-	CardTitle,
-} from "@/components/shadcnui/card";
-import { MinusCircleIcon, PlusCircleIcon } from "lucide-react";
+import BasicCounter from "@/components/BasicCounter";
+import { Card, CardHeader, CardTitle } from "@/components/shadcnui/card";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-	title: "Nextjs Starter Frontend",
-	description: "Production grade Next.js starter template",
+	title: "Basic | Counter State App",
+	description: "Basic Counter State App",
 };
 
 const page = () => {
@@ -22,23 +16,7 @@ const page = () => {
 						Basic Counter
 					</CardTitle>
 				</CardHeader>
-
-				<CardContent className="space-y-4">
-					<div className="text-center text-xl">
-						Count: <span className="font-bold">0</span>
-					</div>
-
-					<div className="grid grid-cols-2 gap-4">
-						<Button
-							variant={"destructive"}
-							className="cursor-pointer">
-							<MinusCircleIcon /> Minus 1
-						</Button>
-						<Button className="cursor-pointer">
-							<PlusCircleIcon /> Plus 1
-						</Button>
-					</div>
-				</CardContent>
+				<BasicCounter />
 			</Card>
 		</section>
 	);
---
+++ src/components/BasicCounter.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { MinusCircleIcon, PlusCircleIcon } from "lucide-react";
+import { Button } from "./shadcnui/button";
+import { CardContent } from "./shadcnui/card";
+import { useState } from "react";
+
+const BasicCounter = () => {
+	// altime logic write here .
+
+	const [count, setCount] = useState(0);
+
+	const plush = () => {
+		setCount(count + 1);
+	};
+
+	const minus = () => {
+		setCount(count - 1);
+	};
+	return (
+		<>
+			<CardContent className="space-y-4">
+				<div className="text-center text-xl">
+					Count: <span className="font-bold">{count}</span>
+				</div>
+
+				<div className="grid grid-cols-2 gap-4">
+					<Button
+						onClick={minus}
+						variant={"destructive"}
+						className="cursor-pointer">
+						<MinusCircleIcon /> Minus 1
+					</Button>
+					<Button
+						onClick={plush}
+						className="cursor-pointer">
+						<PlusCircleIcon /> Plus 1
+					</Button>
+				</div>
+			</CardContent>
+		</>
+	);
+};
+
+export default BasicCounter;
