3bda702098df2929569bfed29d2329e43d2c261d
Mr Sayan <sayansinghachowdhury.dev@gmail.com>
2025-11-27 14:14:50 +0530

    Expert counter part 1 done

---
+++ src/app/expert/page.tsx
@@ -0,0 +1,28 @@
+import AdvanceCounter from "@/components/AdvanceCounter";
+import ExpertCounter from "@/components/ExpertCounter";
+import { Card, CardHeader, CardTitle } from "@/components/shadcnui/card";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Expert | Counter State App",
+	description: "Expert Counter State App",
+};
+
+const page = () => {
+	return (
+		<>
+			<section className="grid h-[90dvh] place-items-center">
+				<Card className="w-xs">
+					<CardHeader>
+						<CardTitle className="text-center text-2xl font-semibold">
+							Expert Counter
+						</CardTitle>
+					</CardHeader>
+					<ExpertCounter />
+				</Card>
+			</section>
+		</>
+	);
+};
+
+export default page;
---
+++ src/components/ExpertCounter.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useState } from "react";
+import { CardContent } from "./shadcnui/card";
+import { Button } from "./shadcnui/button";
+import { MinusCircleIcon, PlusCircleIcon } from "lucide-react";
+
+const ExpertCounter = () => {
+	const [count, setCount] = useState(0);
+
+	const plush = () => {
+		if (count < 100) {
+			setCount(count + 1);
+		}
+	};
+
+	const minus = () => {
+		if (count > 0) {
+			setCount(count - 1);
+		}
+	};
+	const plushTen = () => {
+		if (count <= 90) {
+			setCount(count + 10);
+		}
+	};
+
+	const minusTen = () => {
+		if (count > 9) {
+			setCount(count - 10);
+		}
+	};
+
+	return (
+		<>
+			<CardContent className="space-y-4">
+				<div className="text-center text-xl">
+					Count: <span className="font-bold">{count}</span>
+				</div>
+
+				<div className="grid grid-cols-2 gap-4">
+					<Button
+						onClick={minus}
+						variant={"destructive"}
+						className="cursor-pointer">
+						<MinusCircleIcon /> Minus 1
+					</Button>
+
+					<Button
+						onClick={plush}
+						className="cursor-pointer">
+						<PlusCircleIcon /> Plus 1
+					</Button>
+				</div>
+				<div className="grid grid-cols-2 gap-4">
+					<Button
+						onClick={minusTen}
+						variant={"destructive"}
+						className="cursor-pointer">
+						<MinusCircleIcon /> Minus 10
+					</Button>
+
+					<Button
+						onClick={plushTen}
+						className="cursor-pointer">
+						<PlusCircleIcon /> Plus 10
+					</Button>
+				</div>
+			</CardContent>
+		</>
+	);
+};
+
+export default ExpertCounter;
